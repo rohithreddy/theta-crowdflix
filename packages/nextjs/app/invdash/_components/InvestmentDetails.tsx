59e9586c06c3cba1045e4c85bd8613cddc76578a
crash <chaoscrashx@gmail.com>
2024-07-29 02:31:06 +0530

    TFUEL withdrawal

--- packages/nextjs/app/invdash/_components/InvestmentDetails.tsx
+++ packages/nextjs/app/invdash/_components/InvestmentDetails.tsx
@@ -27,33 +27,35 @@ const InvestmentDetails = (props: Props) => {
 
   return (
     <div className="mt-8 items-center flex flex-col container">
-      <h2 className="text-2xl font-bold p-4">Details of your Investments</h2>
+      <h3 className="text-2xl font-bold p-4">You have earned below TFUEL from the Ticket Sales</h3>
       <div className="gap-8 mt-4">
         <Card className="p-4">
           <CardHeader className="flex items-center justify-center">
-            <CardTitle className="text-center">Total TFUEL Returns</CardTitle>
+            <CardTitle className="text-center">You have earned TFUEL from the Ticket Sales</CardTitle>
           </CardHeader>
-          <CardContent className="flex flex-col mt-2 items-center">
+          <CardContent className="flex flex-col mt-2 items-center justify-center h-full">
             <p className="font-bold text-xl">
               {totalWithDrawableFuelLoading ? (
                 "Loading..."
               ) : totalWithDrawableFuel === 0n ? (
                 "You have no Withdrawable TFUEL"
               ) : (
-                <NumberDisplayToken value={totalWithDrawableFuel as bigint} />
+                <p className="font-bold text-xl">
+                  <NumberDisplayToken value={totalWithDrawableFuel as bigint} /> TFUEL
+                </p>
               )}
             </p>
           </CardContent>
         </Card>
         {/* Render a card for each project ID only if totalWithDrawableFuel is not zero */}
         {totalWithDrawableFuel !== 0n && !fuelWithListLoading && (
-          <div className="gap-8 mt-4 flex flex-wrap items-center justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8 mt-4">
             {withFuellist?.[0].map((projectId, index) => (
-              <Card key={index} className="p-4 basis-1/2 md:basis-1/3 lg:basis-1/4" style={{ height: "auto" }}>
+              <Card key={index} className="p-4 h-64 flex flex-col">
                 <CardHeader className="flex items-center justify-center">
                   <CardTitle className="text-center">Project ID: {projectId.toString()}</CardTitle>
                 </CardHeader>
-                <CardContent className="flex flex-col mt-2 items-center">
+                <CardContent className="flex flex-col mt-2 items-center justify-center h-full">
                   <p className="font-bold text-xl">
                     <NumberDisplayToken value={withFuellist[1][index]} /> TFUEL
                   </p>
